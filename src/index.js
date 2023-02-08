7c343558063cb5f704da6be36c0c3ea8f051fd1f
Makhabat Berdibekova <makhabat.berdibekova.kk@gmail.com>
2023-02-08 20:52:19 +0600

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,11 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if(!matrix) return [];
+
+  return matrix.flatMap((itm, idx) => {
+    if (idx % 2 === 0) {
+        return itm;
+      } else return itm.reverse();
+    });
 }
